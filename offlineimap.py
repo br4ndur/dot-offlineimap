30162df75b0def6b3c651ddf2543bc36657a4bbd
Brandur Øssursson <branduren@gmail.com>
2015-11-09 21:43:27 +0100

    Added email account

--- offlineimap.py
+++ offlineimap.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 import re, os
 
+def get_authinfo_password_ktbit(machine, login, port):
+    s = "machine %s login %s password ([^ ]*) port %s" % (machine, login, port)
+    p = re.compile(s)
+    authinfo = os.popen("gpg -q --no-tty -d ~/.password-store/offlineimap/ktbit.gpg").read()
+    return p.search(authinfo).group(1)
+
 def get_authinfo_password_gmail(machine, login, port):
     s = "machine %s login %s password ([^ ]*) port %s" % (machine, login, port)
     p = re.compile(s)
